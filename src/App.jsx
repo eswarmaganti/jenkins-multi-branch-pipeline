c401077525bc6963f4dc964f1929fa7317c21216
Eswar Krishna Maganti <eswarmaganti@Eswars-MacBook-Air.local>
2024-10-15 11:01:56 +0530

    Updated the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Eswar Krishna Maganti</h1>
+      <h2>DevOps & Automation Developer</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
